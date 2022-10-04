bd9a69deca00f6fd7d3735bcd0aec590b2308be4
Bonnick <bonnickromario@gmail.com>
2022-10-03 22:07:53 -0400

    newly updates

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-    <html>
-     <head> 
-        <meta charset="UTF-8">
-     </head>
-<body>
-    var x="10";
-    if (x===10) {
-      var x=10 
-      console.log(x); 
-    }
-
-    document.write("Hello World");
-   
-    
-
-</body>
-</html>
+window.alert("Hello, world!"); //This window alert is entitled hello, world.
+document.write('Hello, world!<br>');
+var sample="This is a string<br>";
+document.write(sample);
+window.alert(sample);
+document.write("The dog said \"bark\" " + "the cat cried \"meow\"<br>");
+var rain= "concatenated " + "String"
+document.write(rain)
+var animal=" Dog", sky="blue", fire="<br>hot";
+document.write(fire+animal);
+var fire=fire.fontcolor("red"); // The font color red is added to hot
+document.write(fire+animal);
+document.write(3+3);  
