c628914bc1742986dfde211657418c5dd4d278b7
Bonnick <bonnickromario@gmail.com>
2022-10-03 22:41:01 -0400

    finale updates

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,6 +1,7 @@
-function My_First_Function ()  {
-    var str= "This text is green!";
-    var result= str.fontcolor{"green"};
-    document.getElementById{"Green_Text"} .innerHTML=result;
+function myFunction ()  {
+    var sentence= "I am learning";
+    sentence += "a lot from this book!";
+    document.getElementById("concatenate") .innerHTML=sentence;
+    <p id= "concatenate" onclick="myfunction ()"> Click Here</p>
 
 }
