f3cbb5c312dafbb5b881f5d3cbf8bfafaa05f51b
Bonnick <bonnickromario@gmail.com>
2022-10-04 21:10:26 -0400

    changes

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -8,3 +8,26 @@ function multiplication() {
     var simple_Math = 6 * 8;
     document.getElementById("Math1") .innerHTML="6 x 8 = " + simple_Math;
 }
+
+ function division ()  {
+    var simple_Math = 48/6;
+    document.getElementById("Math2") .innerHTML = "48/6 =" + simple_Math;
+ }
+
+ function more_Math() {
+    var simple_Math = (1 +2) * 10/ 2-5;
+    document.getElementById("Math3") .innerHTML = "1 plus 2, multiplied by 10, divided in half and then substracted by 5 equals" + simple_Math;
+ }
+
+ function modulus_Operator() {
+    var simple_Math= 25 % 6;
+    document.getElementById("Math4") .innerHTML= "When you divide 25 by 6 you have a remainder of:" + simple_Math;
+ }
+
+ function negation_Operator() {
+    var X = 10;
+    document.getElementById("Math5") .innerHTML = -X;
+ }
+
+
+
