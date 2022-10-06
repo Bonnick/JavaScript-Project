db2b548b7c6228748a651132a7b3e82e565f8e39
Bonnick <bonnickromario@gmail.com>
2022-10-06 01:01:06 -0400

    need to be revisited

---
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,20 @@
+var X=10;
+function Add_numbers_1() {
+    document.write(20+ X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+
+function Add_number_3() {
+    var X=10;
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_4() {
+    document.write(X + 100);
+}
+Add_numbers_3();
+Add_numbers_4();
