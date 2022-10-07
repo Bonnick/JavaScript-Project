923db952b242af466172f2adda482dedbe5ce510
Bonnick <bonnickromario@gmail.com>
2022-10-06 23:28:54 -0400

    code

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,20 +1,26 @@
 var X=10;
 function Add_numbers_1() {
-    document.write(20+ X + "<br>");
+    document.write(50+ X + "<br>");
 }
 function Add_numbers_2() {
-    document.write(X + 100);
+    document.write(X + 200+ "<br>");
+}
+
+
+
+
+function Add_numbers_3() {
+    var y=10;
+    document.write(20 + y + "<br>");
+}
+function Add_numbers_4() {
+    document.write(y + 100);
 }
 Add_numbers_1();
 Add_numbers_2();
-
-
-function Add_number_3() {
-    var X=10;
-    document.write(20 + X + "<br>");
-}
-function Add_numbers_4() {
-    document.write(X + 100);
-}
 Add_numbers_3();
 Add_numbers_4();
+
+
+ 
+
