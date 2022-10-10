585a752010c478ddd923aeac5b07f715c0cb9859
Bonnick <bonnickromario@gmail.com>
2022-10-10 16:07:41 -0400

    calculator updated

--- Basic JavaScript Projects/Javascript Project/Javascript/Calculator_JavaScript.js
+++ Basic JavaScript Projects/Javascript Project/Javascript/Calculator_JavaScript.js
@@ -57,7 +57,7 @@ function Handle_Operator(Next_Operator) {
         //if operator exist, property lookup is performed for the operator
         //in the perform_Calculation object and the function that matches the
         //operator is executed
-        let result= Perform_Calculation[operator] (Value_Now, Value_of_Input);
+        let result=Perform_Calculation[operator] (Value_Now, Value_of_Input);
         //here we add a fixed amount of numbers after the decimal
         result=Number(result).toFixed(9)
         //this will remove any trailing 0's
@@ -70,7 +70,7 @@ function Handle_Operator(Next_Operator) {
         Calculator.operator= Next_Operator;
 }
 
-const perform_Calculation= {
+const Perform_Calculation= {
     '/':(First_Operand, Second_Operand) => First_Operand / Second_Operand,
     '*':(First_Operand, Second_Operand) => First_Operand * Second_Operand,
     '+':(First_Operand, Second_Operand) => First_Operand + Second_Operand,
